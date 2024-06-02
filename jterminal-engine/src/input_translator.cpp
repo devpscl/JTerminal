803501f8d8aeb44f6cd8b222f42387bbecc8df84
APICode06 <blau.fefran270805@gmail.com>
2024-06-02 22:42:15 +0200

    Update input_translator.cpp

--- jterminal-engine/src/input_translator.cpp
+++ jterminal-engine/src/input_translator.cpp
@@ -1,9 +1,97 @@
 #include "../include/terminput.h"
 
-namespace jterminal {
 
-bool jterminal::translateInput(jterminal::InputEvent *event, jterminal::MemoryBuffer *buffer) {
+bool jterminal::translateInput(jterminal::InputEvent *event, jterminal::StringBuffer* buffer) {
+  if(!buffer->hasNext()) {
+    return false;
+  }
+  auto& esc_buffer = buffer->downcast<ESCBuffer>();
+  event->type = InputType::Unknown;
+  event->symbol = buffer->peekWideChar();
+  size_t len = esc_buffer.scanESCLength();
+  if(len == 0) {
+    len = esc_buffer.peekWideCharLength();
+  }
+  uint8_t bytes[len];
+  esc_buffer.peek(bytes, len);
+  uint64_t hashcode = hashInput(bytes, len);
+  if(translateInput(event, hashcode)) {
+    esc_buffer.skip(len);
+    event->type = InputType::Keyboard;
+    return true;
+  }
+  int param_data[3];
+  uint8_t esf_status;
+  size_t esc_len = esc_buffer.peekSequenceFormat(ESC_CSI, "<#;#;#M", param_data,
+                                                 3, &esf_status);
+  if(esf_status == ESC_FORMAT_OK) {
+    esc_buffer.skip(esc_len);
+    event->mouse.position = pos_t{param_data[1], param_data[2]};
+    switch (param_data[0]) {
+      case 0:
+        event->mouse.button = MOUSE_LEFT_BUTTON;
+        event->mouse.action = MOUSE_ACTION_PRESS;
+        break;
+      case 1:
+        event->mouse.button = MOUSE_WHEEL_BUTTON;
+        event->mouse.action = MOUSE_ACTION_PRESS;
+        break;
+      case 2:
+        event->mouse.button = MOUSE_RIGHT_BUTTON;
+        event->mouse.action = MOUSE_ACTION_PRESS;
+        break;
+      case 64:
+        event->mouse.button = MOUSE_WHEEL_BUTTON;
+        event->mouse.action = MOUSE_ACTION_WHEEL_UP;
+        break;
+      case 65:
+        event->mouse.button = MOUSE_WHEEL_BUTTON;
+        event->mouse.action = MOUSE_ACTION_WHEEL_DOWN;
+        break;
+      case 32:
+        event->mouse.button = MOUSE_LEFT_BUTTON;
+        event->mouse.action = MOUSE_ACTION_MOVE;
+        break;
+      case 33:
+        event->mouse.button = MOUSE_WHEEL_BUTTON;
+        event->mouse.action = MOUSE_ACTION_MOVE;
+        break;
+      case 34:
+        event->mouse.button = MOUSE_RIGHT_BUTTON;
+        event->mouse.action = MOUSE_ACTION_MOVE;
+        break;
+      default:
+        return false;
+    }
+    event->type = InputType::Mouse;
+    return true;
+  }
+  esc_len = esc_buffer.readSequenceFormat(ESC_CSI, "<#;#;#m", param_data,
+                                          3, &esf_status);
+  if(esf_status == ESC_FORMAT_OK) {
+    event->mouse.position = pos_t{param_data[1], param_data[2]};
+    switch (param_data[0]) {
+      case 0:
+        event->mouse.button = MOUSE_LEFT_BUTTON;
+        event->mouse.action = MOUSE_ACTION_RELEASE;
+        break;
+      case 1:
+        event->mouse.button = MOUSE_WHEEL_BUTTON;
+        event->mouse.action = MOUSE_ACTION_RELEASE;
+        break;
+      case 2:
+        event->mouse.button = MOUSE_RIGHT_BUTTON;
+        event->mouse.action = MOUSE_ACTION_RELEASE;
+        break;
+      case 35:
+        event->mouse.button = MOUSE_NONE;
+        event->mouse.action = MOUSE_ACTION_MOVE;
+        break;
+      default:
+        return false;
+    }
+    event->type = InputType::Mouse;
+    return true;
+  }
   return false;
-}
-
 }
