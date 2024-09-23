97c847b0f8a1b4c1c6ebdd83fa533bc2b4661b1a
Pascal <69166882+devpscl@users.noreply.github.com>
2024-09-23 21:18:00 +0200

    Remove unnecessary import

--- jterminal-core/src/main/java/net/jterminal/text/element/TextElementImpl.java
+++ jterminal-core/src/main/java/net/jterminal/text/element/TextElementImpl.java
@@ -3,7 +3,6 @@ package net.jterminal.text.element;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import net.jterminal.text.BackgroundColor;
