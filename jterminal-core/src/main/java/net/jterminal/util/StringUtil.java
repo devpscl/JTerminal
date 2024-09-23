9116416a24514488b2dc70d4b6393cbc8c36bd7c
Pascal <69166882+devpscl@users.noreply.github.com>
2024-09-23 21:17:01 +0200

    Add empty string to utility

--- jterminal-core/src/main/java/net/jterminal/util/StringUtil.java
+++ jterminal-core/src/main/java/net/jterminal/util/StringUtil.java
@@ -4,6 +4,8 @@ import org.jetbrains.annotations.NotNull;
 
 public class StringUtil {
 
+  public static final String EMPTY_STRING = "";
+
   public static @NotNull String unescapeString(@NotNull String str) {
     return str.replaceAll("\033", "\\\\e");
   }
