409e88f87568508b954efe43d2460d27abf45080
Pascal <69166882+devpscl@users.noreply.github.com>
2024-11-04 03:07:16 +0100

    Add EventException

---
+++ jterminal-core/src/main/java/net/jterminal/exception/EventException.java
@@ -0,0 +1,24 @@
+package net.jterminal.exception;
+
+public class EventException extends Exception {
+
+  public EventException() {
+  }
+
+  public EventException(String message) {
+    super(message);
+  }
+
+  public EventException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public EventException(Throwable cause) {
+    super(cause);
+  }
+
+  public EventException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
