cd9ab8c07409052efce34e18a7531406f9f67566
Pascal <69166882+devpscl@users.noreply.github.com>
2024-11-04 15:52:35 +0100

    Update test class

--- jterminal-cli/src/test/java/net/jterminal/test/Main.java
+++ jterminal-cli/src/test/java/net/jterminal/test/Main.java
@@ -5,11 +5,24 @@ import net.jterminal.cli.CLITerminal;
 import net.jterminal.cli.CLITerminalProvider;
 import net.jterminal.cli.history.InputHistory;
 import net.jterminal.cli.line.LineReader;
+import net.jterminal.cli.line.PrefixedLineRenderer;
 import net.jterminal.cli.util.RecordingBuffer;
 import net.jterminal.exception.TerminalInitializeException;
 import net.jterminal.exception.TerminalProviderException;
+import net.jterminal.text.TerminalColor;
+import net.jterminal.text.termstring.TermString;
+import org.jetbrains.annotations.NotNull;
 
 public class Main {
+
+  public static @NotNull TermString prefix() {
+    return TermString.builder()
+        .foregroundColor(TerminalColor.GREEN)
+        .append("$user")
+        .resetStyle()
+        .append(": ")
+        .build();
+  }
 
   public static void main(String[] args)
       throws TerminalInitializeException, TerminalProviderException {
@@ -17,9 +30,11 @@ public class Main {
     Terminal.set(terminal);
     terminal.lineReading(true);
     LineReader lineReader = terminal.lineReader();
+    lineReader.lineRenderer(new PrefixedLineRenderer(Main::prefix));
     lineReader.addFlags(LineReader.FLAG_PRINT_LEGACY);
     lineReader.removeFlags(LineReader.FLAG_INSERT_MODE);
     lineReader.inputHistory(InputHistory.create(false));
+    lineReader.updateDisplay();
     terminal.setOutputRecordingBuffer(new RecordingBuffer(3800, 4096));
     terminal.lineReader().inputHistory(InputHistory.create(true));
     Thread t = new Thread(() -> {
@@ -31,7 +46,7 @@ public class Main {
         }
         terminal.writeLine("Test: " + i);
         if(i == 6) {
-          terminal.clear();
+          terminal.restoreRecordedOutput();
         }
       }
     });
