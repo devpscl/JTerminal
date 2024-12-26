42c366c01fd3b1668105efa53048fec9fdb91841
Pascal <69166882+devpscl@users.noreply.github.com>
2024-12-26 18:05:26 +0100

    Update string utility

--- jterminal-core/src/main/java/net/jterminal/util/StringUtil.java
+++ jterminal-core/src/main/java/net/jterminal/util/StringUtil.java
@@ -1,6 +1,8 @@
 package net.jterminal.util;
 
+import java.util.Arrays;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class StringUtil {
 
@@ -8,6 +10,35 @@ public class StringUtil {
 
   public static @NotNull String unescapeString(@NotNull String str) {
     return str.replaceAll("\033", "\\\\e");
+  }
+
+  public static @NotNull String notNull(@Nullable String str) {
+    return notNull(str, "null");
+  }
+
+  public static @NotNull String notNull(@Nullable String str, @NotNull String alt) {
+    return str == null ? alt : str;
+  }
+
+  public static char resolveEscapeChar(char suffix) {
+    return switch (suffix) {
+      case 'n' -> '\n';
+      case '"' -> '\"';
+      case '\'' -> '\'';
+      case 'e' -> 27;
+      case 't' -> '\t';
+      case 'f' -> '\f';
+      case 'b' -> '\b';
+      case 'r' -> '\r';
+      case '\\' -> '\\';
+      default -> throw new IllegalArgumentException("Invalid escape character: " + suffix);
+    };
+  }
+
+  public static @NotNull String repeat(char c, int count) {
+    char[] arr = new char[count];
+    Arrays.fill(arr, c);
+    return new String(arr);
   }
 
 }
