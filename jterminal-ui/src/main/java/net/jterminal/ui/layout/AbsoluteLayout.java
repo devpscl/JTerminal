9098f8d698b2116b972c61ff8f4b7c95cf6b49a3
Pascal <69166882+devpscl@users.noreply.github.com>
2024-12-30 19:02:02 +0100

    Add absolute layout

---
+++ jterminal-ui/src/main/java/net/jterminal/ui/layout/AbsoluteLayout.java
@@ -0,0 +1,30 @@
+package net.jterminal.ui.layout;
+
+import net.jterminal.ui.component.Component;
+import net.jterminal.util.TerminalDimension;
+import net.jterminal.util.TerminalPosition;
+import org.jetbrains.annotations.NotNull;
+
+public class AbsoluteLayout implements Layout {
+
+
+  @Override
+  public void addComponent(@NotNull Component component) {
+  }
+
+  @Override
+  public void removeComponent(@NotNull Component component) {
+  }
+
+  @Override
+  public @NotNull TerminalPosition move(@NotNull Component component,
+      @NotNull TerminalDimension containerSize) {
+    return component.position();
+  }
+
+  @Override
+  public @NotNull TerminalDimension resize(@NotNull Component component,
+      @NotNull TerminalDimension containerSize) {
+    return component.size();
+  }
+}
---
+++ jterminal-ui/src/main/java/net/jterminal/ui/layout/Layout.java
@@ -0,0 +1,21 @@
+package net.jterminal.ui.layout;
+
+import net.jterminal.ui.component.Component;
+import net.jterminal.util.TerminalDimension;
+import net.jterminal.util.TerminalPosition;
+import org.jetbrains.annotations.NotNull;
+
+public interface Layout {
+
+  void addComponent(@NotNull Component component);
+
+  void removeComponent(@NotNull Component component);
+
+  @NotNull TerminalPosition move(@NotNull Component component,
+      @NotNull TerminalDimension containerSize);
+
+  @NotNull TerminalDimension resize(@NotNull Component component,
+      @NotNull TerminalDimension containerSize);
+
+
+}
