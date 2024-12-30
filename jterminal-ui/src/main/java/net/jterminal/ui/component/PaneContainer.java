3d7366fbe9e586b8c7266d8989c04e289310355d
Pascal <69166882+devpscl@users.noreply.github.com>
2024-12-30 19:03:31 +0100

    Add pane container

---
+++ jterminal-ui/src/main/java/net/jterminal/ui/component/PaneContainer.java
@@ -0,0 +1,12 @@
+package net.jterminal.ui.component;
+
+import net.jterminal.ui.graphics.TermGraphics;
+import org.jetbrains.annotations.NotNull;
+
+public class PaneContainer extends Container {
+
+  @Override
+  public void paint(@NotNull TermGraphics graphics) {
+
+  }
+}
