285c4f1334a3373fe83ce0542eea4a8ff5d5aac5
Pascal <69166882+devpscl@users.noreply.github.com>
2024-12-30 19:02:36 +0100

    Add label

---
+++ jterminal-ui/src/main/java/net/jterminal/ui/component/Label.java
@@ -0,0 +1,52 @@
+package net.jterminal.ui.component;
+
+import net.jterminal.text.termstring.TermString;
+import net.jterminal.ui.graphics.TermGraphics;
+import net.jterminal.util.TerminalDimension;
+import org.jetbrains.annotations.NotNull;
+
+public class Label extends Component {
+
+  private TermString text;
+
+  public Label(@NotNull TermString text) {
+    this.text = text;
+    updateSize();
+  }
+
+  public Label() {
+    this(TermString.empty());
+  }
+
+  private void updateSize() {
+    int width = 0;
+    TermString[] lines = text.split('\n');
+    int height = lines.length;
+    for (TermString line : lines) {
+      if(line.length() > width) {
+        width = line.length();
+      }
+    }
+    size = new TerminalDimension(width, height);
+  }
+
+  public @NotNull TermString text() {
+    return text;
+  }
+
+  public void text(@NotNull TermString text) {
+    this.text = text;
+    updateSize();
+    repaint();
+  }
+
+  @Override
+  public void size(@NotNull TerminalDimension size) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void paint(@NotNull TermGraphics graphics) {
+    graphics.drawString(1, 1, text);
+  }
+}
