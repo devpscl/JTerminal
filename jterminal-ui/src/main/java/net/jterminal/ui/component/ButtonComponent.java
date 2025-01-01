118bd01d3df1629e9a4ee69574de42effa127071
Pascal <69166882+devpscl@users.noreply.github.com>
2025-01-01 17:01:31 +0100

    Add button component

---
+++ jterminal-ui/src/main/java/net/jterminal/ui/component/ButtonComponent.java
@@ -0,0 +1,73 @@
+package net.jterminal.ui.component;
+
+import net.jterminal.input.Keyboard;
+import net.jterminal.input.KeyboardInputEvent;
+import net.jterminal.text.TerminalColor;
+import net.jterminal.ui.event.component.ComponentKeyEvent;
+import net.jterminal.ui.graphics.TermGraphics;
+import net.jterminal.util.TerminalDimension;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ButtonComponent extends Component implements Selectable {
+
+  private String text;
+  private Runnable action;
+
+  public ButtonComponent() {
+    this("Button");
+  }
+
+  public ButtonComponent(@NotNull String text) {
+    text(text);
+  }
+
+  public @NotNull String text() {
+    return text;
+  }
+
+  public @Nullable Runnable action() {
+    return action;
+  }
+
+  public void action(@Nullable Runnable action) {
+    this.action = action;
+  }
+
+  public void text(@NotNull String text) {
+    this.text = text;
+    size = new TerminalDimension(text.length() + 2, 1);
+  }
+
+  @Override
+  public void size(@NotNull TerminalDimension size) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void paint(@NotNull TermGraphics graphics) {
+    if(isSelected()) {
+      TerminalColor foregroundColor = TerminalColor.from(graphics.foregroundColor());
+      TerminalColor backgroundColor = TerminalColor.from(graphics.backgroundColor());
+      graphics.foregroundColor(backgroundColor);
+      graphics.backgroundColor(foregroundColor);
+    }
+    graphics.drawString(1, 1, "[" + text+ "]");
+  }
+
+  @Override
+  public void processKeyEvent(@NotNull ComponentKeyEvent event) {
+    super.processKeyEvent(event);
+    if(!isSelected()) {
+      return;
+    }
+    if(event.key() == Keyboard.KEY_ENTER && action != null) {
+      action.run();
+    }
+  }
+
+  @Override
+  public boolean allowActionInput(@NotNull KeyboardInputEvent event) {
+    return event.key() != Keyboard.KEY_ENTER;
+  }
+}
