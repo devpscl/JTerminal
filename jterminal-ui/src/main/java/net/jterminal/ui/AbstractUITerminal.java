33be0d2311fb3a245f5669abdbc1ecb640edd2e7
Pascal <69166882+devpscl@users.noreply.github.com>
2025-01-01 18:07:24 +0100

    Move classes to selectable package

--- jterminal-ui/src/main/java/net/jterminal/ui/AbstractUITerminal.java
+++ jterminal-ui/src/main/java/net/jterminal/ui/AbstractUITerminal.java
@@ -13,7 +13,7 @@ import net.jterminal.instance.AbstractNativeTerminal;
 import net.jterminal.text.BackgroundColor;
 import net.jterminal.text.ForegroundColor;
 import net.jterminal.ui.component.Component;
-import net.jterminal.ui.component.Selectable;
+import net.jterminal.ui.component.selectable.Selectable;
 import net.jterminal.ui.event.ScreenCloseEvent;
 import net.jterminal.ui.event.ScreenOpenEvent;
 import net.jterminal.ui.event.ScreenRenderedEvent;
