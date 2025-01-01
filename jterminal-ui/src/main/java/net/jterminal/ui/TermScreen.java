770a62ead7099f8d9805a70aca05883dc18676fb
Pascal <69166882+devpscl@users.noreply.github.com>
2025-01-01 17:01:51 +0100

    Add selection listeners

--- jterminal-ui/src/main/java/net/jterminal/ui/TermScreen.java
+++ jterminal-ui/src/main/java/net/jterminal/ui/TermScreen.java
@@ -1,16 +1,22 @@
 package net.jterminal.ui;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import net.jterminal.ui.component.Component;
-import net.jterminal.ui.component.ComponentGraphics;
-import net.jterminal.ui.component.Container;
-import net.jterminal.ui.graphics.TermGraphics;
+import net.jterminal.ui.component.PaneContainer;
+import net.jterminal.ui.event.component.ComponentSelectEvent;
+import net.jterminal.ui.event.component.ComponentUnselectEvent;
 import net.jterminal.util.TerminalDimension;
 import net.jterminal.util.TerminalPosition;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class TermScreen extends Container {
+public class TermScreen extends PaneContainer {
 
   UITerminal terminal;
+  private Component selectedComponent = null;
 
   @Override
   public void repaint() {
@@ -22,15 +28,10 @@ public class TermScreen extends Container {
 
   @Override
   public void repaintFully() {
-    repaint();
-  }
-
-  @Override
-  public void paint(@NotNull TermGraphics graphics) {
-    for (Component component : components()) {
-      component.updatePositionSize();
-      ComponentGraphics.draw(graphics, component);
+    if(!isOpened()) {
+      return;
     }
+    terminal.drawNewScreen();
   }
 
   public boolean isOpened() {
@@ -62,4 +63,72 @@ public class TermScreen extends Container {
   public @NotNull TerminalPosition effectivePosition() {
     return new TerminalPosition(1, 1);
   }
+
+  public void select(@NotNull Component selectedComponent) {
+    if(this.selectedComponent != null) {
+      selectedComponent.eventBus().post(new ComponentUnselectEvent());
+    }
+    this.selectedComponent = selectedComponent;
+    selectedComponent.eventBus().post(new ComponentSelectEvent());
+    repaint();
+  }
+
+  public void unselect() {
+    if(selectedComponent != null) {
+      selectedComponent.eventBus().post(new ComponentUnselectEvent());
+      selectedComponent = null;
+      repaint();
+    }
+  }
+
+  public @Nullable Component selectedComponent() {
+    return selectedComponent;
+  }
+
+  public void performSelect(int x, int y) {
+    Collection<Component> components = deepSelectableComponents();
+    if(components.isEmpty()) {
+      unselect();
+      return;
+    }
+    List<Component> priorityComponents = new ArrayList<>(components);
+    Collections.reverse(priorityComponents);
+    for (Component component : priorityComponents) {
+      if(component.contains(x, y)) {
+        if(component == selectedComponent) {
+          return;
+        }
+        select(component);
+        return;
+      }
+    }
+    unselect();
+  }
+
+  public void selectNext() {
+    Collection<Component> components = deepSelectableComponents();
+    if(components.isEmpty()) {
+      unselect();
+      return;
+    }
+    boolean next = false;
+    for (Component component : components) {
+      if(selectedComponent == null) {
+        select(component);
+        return;
+      }
+      if(component == selectedComponent) {
+        next = true;
+        continue;
+      }
+      if(next) {
+        select(component);
+        return;
+      }
+    }
+    if(next) {
+      unselect();
+    }
+  }
+
 }
