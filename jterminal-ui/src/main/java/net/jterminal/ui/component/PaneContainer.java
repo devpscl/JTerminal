5bf48fc53fcb9c73fa4a3f435cf9cb67f112b893
Pascal <69166882+devpscl@users.noreply.github.com>
2025-01-12 12:32:50 +0100

    Update PaneContainer

--- jterminal-ui/src/main/java/net/jterminal/ui/component/PaneContainer.java
+++ jterminal-ui/src/main/java/net/jterminal/ui/component/PaneContainer.java
@@ -1,14 +1,9 @@
 package net.jterminal.ui.component;
 
 import net.jterminal.ui.graphics.TermGraphics;
-import net.jterminal.util.TermDim;
 import org.jetbrains.annotations.NotNull;
 
 public class PaneContainer extends Container implements Resizeable {
-
-  private TermDim minimumSize = new TermDim(1, 1);
-  private TermDim maximumSize = new TermDim(Integer.MAX_VALUE,
-      Integer.MAX_VALUE);
 
   @Override
   public void paint(@NotNull TermGraphics graphics, @NotNull Component component) {
@@ -16,21 +11,5 @@ public class PaneContainer extends Container implements Resizeable {
     ComponentGraphics.draw(graphics, component);
   }
 
-  @Override
-  public @NotNull TermDim minimumSize() {
-    return minimumSize;
-  }
 
-  @Override
-  public @NotNull TermDim maximumSize() {
-    return maximumSize;
-  }
-
-  public void minimumSize(@NotNull TermDim minimumSize) {
-    this.minimumSize = minimumSize;
-  }
-
-  public void maximumSize(@NotNull TermDim maximumSize) {
-    this.maximumSize = maximumSize;
-  }
 }
