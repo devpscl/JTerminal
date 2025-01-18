74891de817e5f227e729f2da731db3dbaa9034cc
Pascal <69166882+devpscl@users.noreply.github.com>
2025-01-18 16:14:58 +0100

    Update arch values

--- jterminal-core/src/main/java/net/jterminal/system/ProcessorArchitecture.java
+++ jterminal-core/src/main/java/net/jterminal/system/ProcessorArchitecture.java
@@ -3,34 +3,35 @@ package net.jterminal.system;
 import org.jetbrains.annotations.NotNull;
 
 public enum ProcessorArchitecture {
-
   ARCHITECTURE_X86("x86"),
-  ARCHITECTURE_X86_64("x86_64", true),
-  ARCHITECTURE_AMD64("amd64", true),
+  ARCHITECTURE_X86_64(true, "x86_64", "amd64"),
   ARCHITECTURE_ARM("arm"),
-  ARCHITECTURE_AARCH64("aarch64", true),
+  ARCHITECTURE_ARM64(true, "aarch64", "arm64"),
   ARCHITECTURE_PPC("ppc"),
-  ARCHITECTURE_PPC64("ppc64", true),
-  ARCHITECTURE_PPC64LE("ppc64le", true),
+  ARCHITECTURE_PPC64(true, "ppc64"),
   ARCHITECTURE_SPARC("sparc"),
-  ARCHITECTURE_SPARCV9("sparcv9", true),
+  ARCHITECTURE_SPARCV9(true, "sparcv9"),
   ARCHITECTURE_MIPS("mips"),
-  ARCHITECTURE_MIPS64("mips64", true),
-  ARCHITECTURE_MIPS64EL("mips64el", true),
-  ARCHITECTURE_POWERPC("PowerPC"),
-  ARCHITECTURE_IA64("ia64", true),
-  UNKNOWN("");
+  ARCHITECTURE_MIPS64(true, "mips64"),
+  ARCHITECTURE_IA64(true, "ia64"),
+  UNKNOWN("unknown");
 
+  private final String[] names;
   private final String mnemonic;
   private final boolean is64Bit;
 
-  ProcessorArchitecture(final String mnemonic) {
-    this(mnemonic, false);
+  ProcessorArchitecture(String...names) {
+    this(false, names);
   }
 
-  ProcessorArchitecture(final String mnemonic, boolean is64Bit) {
-    this.mnemonic = mnemonic;
+  ProcessorArchitecture(boolean is64Bit, String...names) {
+    this.mnemonic = names[0];
     this.is64Bit = is64Bit;
+    this.names = names;
+  }
+
+  public String[] names() {
+    return names;
   }
 
   public boolean is64Bit() {
