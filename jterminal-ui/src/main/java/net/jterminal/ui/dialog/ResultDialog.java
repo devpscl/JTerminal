30a20a1dbd61036238730fe4b5ed8321ccffd5a2
Pascal <69166882+devpscl@users.noreply.github.com>
2025-01-26 20:24:25 +0100

    Add result dialog

---
+++ jterminal-ui/src/main/java/net/jterminal/ui/dialog/ResultDialog.java
@@ -0,0 +1,152 @@
+package net.jterminal.ui.dialog;
+
+import java.util.function.Consumer;
+import net.jterminal.Terminal;
+import net.jterminal.text.termstring.TermString;
+import net.jterminal.ui.component.FrameContainer;
+import net.jterminal.ui.component.LabelComponent;
+import net.jterminal.ui.component.selectable.ButtonComponent;
+import net.jterminal.ui.layout.Anchor;
+import net.jterminal.ui.layout.Layout;
+import net.jterminal.ui.util.BoxCharacter.Type;
+import net.jterminal.ui.util.TextAlignment;
+import org.jetbrains.annotations.NotNull;
+
+public class ResultDialog extends TermDialog {
+
+  public enum OptionType {
+    YES_NO_TYPE,
+    YES_NO_CANCEL_TYPE,
+    OK_TYPE,
+    OK_CANCEL_TYPE
+  }
+
+  public static final int BUTTON_OK = 1;
+  public static final int BUTTON_YES = 2;
+  public static final int BUTTON_NO = 3;
+  public static final int BUTTON_CANCEL = 4;
+
+  private final OptionType type;
+  private Consumer<Integer> action = null;
+  private final FrameContainer frameContainer = new FrameContainer();
+  private final LabelComponent labelComponent = new LabelComponent();
+
+  public ResultDialog(@NotNull String title, OptionType type) {
+    this.type = type;
+    frameContainer.title(title);
+    init();
+  }
+
+  public @NotNull String title() {
+    return frameContainer.title();
+  }
+
+  public void title(@NotNull String title) {
+    frameContainer.title(title);
+  }
+
+  public @NotNull TermString message() {
+    return labelComponent.text();
+  }
+
+  public void message(@NotNull TermString message) {
+    labelComponent.text(message, true);
+    labelComponent.width(Layout.fill());
+  }
+
+  public void action(@NotNull Consumer<Integer> action) {
+    this.action = action;
+  }
+
+  public @NotNull OptionType optionType() {
+    return type;
+  }
+
+  private void init() {
+    frameContainer.boxLineType(Type.DOUBLE);
+    frameContainer.titleAlignment(TextAlignment.CENTER);
+    frameContainer.width(Layout.fill());
+    frameContainer.height(Layout.fill());
+
+    labelComponent.textAlignment(TextAlignment.CENTER);
+    labelComponent.y(Layout.relative(Anchor.TOP), Layout.offset(1));
+    labelComponent.x(1);
+    labelComponent.width(Layout.fill());
+    frameContainer.add(labelComponent);
+
+    ButtonComponent okButton = new ButtonComponent("OK");
+    okButton.action(this::handleOkButton);
+
+    ButtonComponent yesButton = new ButtonComponent("YES");
+    yesButton.action(this::handleYesButton);
+
+    ButtonComponent noButton = new ButtonComponent("NO");
+    noButton.action(this::handleNoButton);
+
+    ButtonComponent cancelButton = new ButtonComponent("CANCEL");
+    cancelButton.action(this::handleCancelButton);
+
+    switch (type) {
+      case OK_TYPE -> addButtons(okButton);
+      case OK_CANCEL_TYPE -> addButtons(okButton, cancelButton);
+      case YES_NO_TYPE -> addButtons(yesButton, noButton);
+      case YES_NO_CANCEL_TYPE -> addButtons(yesButton, noButton, cancelButton);
+    }
+    add(frameContainer);
+  }
+
+  private void addButtons(ButtonComponent...buttonComponents) {
+    int length = 0;
+    for (ButtonComponent buttonComponent : buttonComponents) {
+      length += buttonComponent.text().length() + 2 + 1;
+    }
+
+    if(length < 1) {
+      return;
+    }
+    int halfLength = length / 2;
+    ButtonComponent lastComponent = null;
+    for (ButtonComponent buttonComponent : buttonComponents) {
+      buttonComponent.y(Layout.relative(Anchor.BOTTOM));
+      if(lastComponent == null) {
+        buttonComponent.x(Layout.relative(Anchor.RIGHT), Layout.scale(0.5F),
+            Layout.offset(-halfLength));
+        frameContainer.add(buttonComponent);
+        lastComponent = buttonComponent;
+        continue;
+      }
+      buttonComponent.x(Layout.relative(lastComponent, Anchor.RIGHT), Layout.offset(1));
+      lastComponent = buttonComponent;
+      frameContainer.add(buttonComponent);
+    }
+  }
+
+  private void handleOkButton() {
+    if(action != null) {
+      action.accept(BUTTON_OK);
+    }
+    closeDialog();
+  }
+
+  private void handleYesButton() {
+    if(action != null) {
+      action.accept(BUTTON_YES);
+    }
+    closeDialog();
+  }
+
+  private void handleNoButton() {
+    if(action != null) {
+      action.accept(BUTTON_NO);
+    }
+    closeDialog();
+  }
+
+  private void handleCancelButton() {
+    if(action != null) {
+      action.accept(BUTTON_CANCEL);
+    }
+    closeDialog();
+  }
+
+}
---
+++ jterminal-ui/src/main/java/net/jterminal/ui/dialog/ResultDialogBuilder.java
@@ -0,0 +1,84 @@
+package net.jterminal.ui.dialog;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import net.jterminal.text.termstring.TermString;
+import net.jterminal.ui.dialog.ResultDialog.OptionType;
+import net.jterminal.ui.layout.Layout;
+import org.jetbrains.annotations.NotNull;
+
+public class ResultDialogBuilder {
+
+  private String title;
+  private TermString message;
+  private ResultDialog.OptionType optionType;
+  private float widthScale = 0.5F;
+  private float heightScale = 0.2F;
+  private final Map<Integer, Consumer<ResultDialog>> eventMap = new HashMap<>();
+
+  public @NotNull String title() {
+    return title;
+  }
+
+  public @NotNull ResultDialogBuilder title(String title) {
+    this.title = title;
+    return this;
+  }
+
+  public @NotNull TermString message() {
+    return message;
+  }
+
+  public @NotNull ResultDialogBuilder message(@NotNull TermString message) {
+    this.message = message;
+    return this;
+  }
+
+  public @NotNull OptionType optionType() {
+    return optionType;
+  }
+
+  public @NotNull ResultDialogBuilder optionType(@NotNull OptionType optionType) {
+    this.optionType = optionType;
+    return this;
+  }
+
+  public float widthScale() {
+    return widthScale;
+  }
+
+  public @NotNull ResultDialogBuilder widthScale(float scale) {
+    this.widthScale = scale;
+    return this;
+  }
+
+  public float heightScale() {
+    return heightScale;
+  }
+
+  public @NotNull ResultDialogBuilder heightScale(float scale) {
+    this.heightScale = scale;
+    return this;
+  }
+
+  public @NotNull ResultDialogBuilder event(int button, @NotNull Consumer<ResultDialog> event) {
+    eventMap.put(button, event);
+    return this;
+  }
+
+  public @NotNull ResultDialog build() {
+    final ResultDialog dialog = new ResultDialog(title, optionType);
+    dialog.message(message);
+    dialog.width(Layout.fill(), Layout.scale(widthScale));
+    dialog.height(Layout.fill(), Layout.scale(heightScale));
+    dialog.action(intValue -> {
+      Consumer<ResultDialog> resultDialogConsumer = eventMap.get(intValue);
+      if(resultDialogConsumer != null) {
+        resultDialogConsumer.accept(dialog);
+      }
+    });
+    return dialog;
+  }
+
+}
